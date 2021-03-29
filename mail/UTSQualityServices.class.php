31f4a4bda4e67ad4d7be3cc420bfff5d2dd1bbe5
keith blackwelder <nolimits1120@gmail.com>
2021-03-29 13:44:35 -0400

    big commit

---
+++ mail/UTSQualityServices.class.php
@@ -0,0 +1,35 @@
+<?php
+class PrimaryClass
+{
+    public function __construct()
+    {
+    }
+
+    public function doAction()
+    {
+        $action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING) ?: "";
+        // Process the Action.
+
+        if (strpos($action, ".") === false) {
+            $method = $action;
+            if (method_exists(__CLASS__, $method)) {
+                $this->$method();
+                return;
+            }
+        } else {
+            $actionParts = explode(".", $action);
+            $class = (isset($actionParts[0]) ? $actionParts[0] : "");
+            $method = (isset($actionParts[1]) ? $actionParts[1] : "");
+            if (method_exists($class, $method)) {
+                $class = new $class();
+                $class->$method();
+                $class = null;
+                return;
+            }
+        }
+    }
+
+    public function testing() {
+        echo "Hello, World! Testing phase one complete.";
+    }
+}
---
+++ mail/starter.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'UTSQualityServices.class.php';
+
+$obj = new Primary;
+$obj->doAction();

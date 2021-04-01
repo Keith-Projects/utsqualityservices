80d726f4dabbd2dc2f9ca672fc8104d0eb3fefba
keith blackwelder <nolimits1120@gmail.com>
2021-04-01 07:33:37 -0400

    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['message'])) {
+if (empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['message'])) {
   echo "empty array";
   //exit();
 }
@@ -13,7 +13,10 @@ $formId = strip_tags(htmlspecialchars($_POST['formId']));
 // Create the email and send the message
 $to = "keith.blackwelder@ktbwebservices.com";
 $subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nFormID: $formId\n\nMessage:\n$message";
+$body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nPhone: $phone\n\nFormID: $formId\n\nMessage:\n$message";
 
-if(!mail($to, $subject, $body))
+if (!mail($to, $subject, $body)) {
   http_response_code(500);
+} else {
+  echo "EMAIL WAS SENT";
+}

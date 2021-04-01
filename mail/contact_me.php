4c3f5a642cae1b3f73fd4dd478865e4c59562463
keith blackwelder <nolimits1120@gmail.com>
2021-04-01 07:20:15 -0400

    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,8 @@
 <?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['message'])) {
-  http_response_code(500);
-  exit();
+  echo "empty array";
+  //exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -15,5 +15,5 @@ $to = "keith.blackwelder@ktbwebservices.com";
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nFormID: $formId\n\nMessage:\n$message";
 
-if(!mail($to, $subject, $body, "From: keith.blackwelder@ktbwebservices.com"))
+if(!mail($to, $subject, $body))
   http_response_code(500);

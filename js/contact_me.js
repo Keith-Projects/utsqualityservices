54eecafe16ed65d51f3e665eb78bb543210f1e6c
keith blackwelder <nolimits1120@gmail.com>
2021-05-31 10:03:09 -0400

    fixed issue with contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,9 +11,23 @@ $(function () {
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
+
+      $("#success").empty();
+
       // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(" ") >= 0) {
-        firstName = name.split(" ").slice(0, -1).join(" ");
+      if (firstName.trim() == "") {
+        $("#contactForm").trigger("reset");
+        $("#success").html("<div class='alert alert-danger'>");
+        $("#success > .alert-danger")
+          .html(
+            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+          )
+          .append("</button>");
+        $("#success > .alert-danger").append(
+          $("<strong>").text("The username can not be empty.")
+        );
+        $("#success > .alert-danger").append("</div>");
+        return false;
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
